144ba9d5f0e3085fce0ee38ab9dcecae979c11ac
fberal <148898675+fberal@users.noreply.github.com>
2024-02-11 19:38:02 +0000

    mise à jour test01.py

--- test01.py
+++ test01.py
@@ -1,1 +1,13 @@
-print('Hello World!')
+liste_vide = []
+string_vide = ''
+liste_non_vide = [1, 2]
+string_non_vide = 'hey'
+zéro = 0
+pas_zéro = -0.01
+
+print(liste_vide, bool(liste_vide))
+print(string_vide, bool(string_vide))
+print(liste_non_vide, bool(liste_non_vide))
+print(string_non_vide, bool(string_non_vide))
+print(zéro, bool(zéro))
+print(pas_zéro, bool(pas_zéro))
